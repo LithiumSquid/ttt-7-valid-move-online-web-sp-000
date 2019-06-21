21ae06767f3a8b9b875b13552991e9ec3301fae6
Matthew Lewandowski <matthew.lewandowski@wework.com>
2019-06-21 17:48:07 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  if index.between?(0,8) && position_taken? == TRUE
+  if index.between?(0,8) && position_taken?(board, index) == TRUE
     return TRUE
   else
     return FALSE
